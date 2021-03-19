208caedae6b416f87de9974586ab9c6a89ac6495
MintMissy <dawidkostka480@gmail.com>
2021-03-19 21:42:20 +0100

    add generate class

---
+++ scripts/generators.js
@@ -0,0 +1,13 @@
+function generateFooter(){
+    let generateLocation = document.getElementsByClassName("footer")[0];
+    generateLocation.innerHTML = "" +
+        "    <p class=\"footerInfo primaryGray text\"><a class=\"link\" href=\"https://www.youtube.com/channel/UCx-6vO4gFYume-eMa_OMJ6w\">YouTube</a></p>\n" +
+        "    <p class=\"footerInfo primaryGray text\"><a class=\"link\" href=\"https://www.instagram.com/mint.missy/\">Instagram</a></p>\n" +
+        "    <p class=\"footerInfo primaryGray text\"><a class=\"link\" href=\"https://github.com/MintMissy\">Github</a></p>\n" +
+        "    <hr class=\"lineFooter\">\n" +
+        "    <p class=\"primaryGray text footerCredits\" style=\"text-align: center\">Designed by <a class=\"link\" href=\"https://github.com/MintMissy\">Mint Missy</a></p>"
+}
+
+function generateNavbar(){
+
+}
