2d5ffda92f2f9a9fa85304fe2088d057c5931df2
MintMissy <dawidkostka480@gmail.com>
2021-03-20 12:13:59 +0100

    Add navbar generator

--- scripts/generators.js
+++ scripts/generators.js
@@ -1,4 +1,4 @@
-function generateFooter(){
+function generateFooter() {
     let generateLocation = document.getElementsByClassName("footer")[0];
     generateLocation.innerHTML = "" +
         "    <p class=\"footerInfo primaryGray text\"><a class=\"link\" href=\"https://www.youtube.com/channel/UCx-6vO4gFYume-eMa_OMJ6w\">YouTube</a></p>\n" +
@@ -8,6 +8,42 @@ function generateFooter(){
         "    <p class=\"primaryGray text footerCredits\" style=\"text-align: center\">Designed by <a class=\"link\" href=\"https://github.com/MintMissy\">Mint Missy</a></p>"
 }
 
-function generateNavbar(){
+function generateNavbar(siteType, pathToMain) {
+    let generateLocation = document.getElementById("navbar");
 
+    generateLocation.innerHTML = "" +
+    "    <div class=\"buttonLeft\">\n" +
+    "        <div class=\"primaryColor navbarText logoButton\">\n" +
+    "            <img id=\"navbarLogo\" src=\"" + pathToMain + "images/logo.svg\" alt=\"logo\">\n" +
+    "        </div>\n" +
+    "    </div>\n" +
+    "    <a href=\"" + pathToMain + "contact.html\">\n" +
+    "        <div id=\"contact\" class=\"buttonRight contactButton\">\n" +
+    "            <div class=\"primaryColor navbarText textButton\">CONTACT</div>\n" +
+    "        </div>\n" +
+    "    </a>\n" +
+    "    <a href=\"" + pathToMain + "about.html\">\n" +
+    "        <div id=\"about\" class=\"buttonRight aboutButton\">\n" +
+    "            <div class=\"primaryColor navbarText textButton\">ABOUT</div>\n" +
+    "        </div>\n" +
+    "    </a>\n" +
+    "    <a href=\"" + pathToMain + "games.html\">\n" +
+    "        <div id=\"games\" class=\"buttonRight gamesButton\">\n" +
+    "            <div class=\"primaryColor navbarText textButton\">GAMES</div>\n" +
+    "        </div>\n" +
+    "    </a>\n" +
+    "    <a href=\"" + pathToMain + "tools.html\">\n" +
+    "        <div id=\"tools\" class=\"buttonRight toolsButton\">\n" +
+    "            <div class=\"primaryColor navbarText textButton\">TOOLS</div>\n" +
+    "        </div>\n" +
+    "    </a>\n" +
+    "\n" +
+    "    <a href=\"" + pathToMain + "index.html\">\n" +
+    "        <div id=\"home\" class=\"buttonLeft homeButton\">\n" +
+    "            <div class=\"primaryColor navbarText textButton\">HOME</div>\n" +
+    "        </div>\n" +
+    "    </a>\n"
+
+    // Add active site class
+    document.getElementById(siteType).classList += " activeSite"
 }
