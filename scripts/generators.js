6678686a57ba506d9bb643b2ea84e251609767b2
MintMissy <dawidkostka480@gmail.com>
2021-03-20 17:31:38 +0100

    Fix navigation bug

--- scripts/generators.js
+++ scripts/generators.js
@@ -8,37 +8,37 @@ function generateFooter() {
         "    <p class=\"primaryGray text footerCredits\" style=\"text-align: center\">Designed by <a class=\"link\" href=\"https://github.com/MintMissy\">Mint Missy</a></p>"
 }
 
-function generateNavbar(siteType, pathToMain) {
+function generateNavbar(siteType, pathToPages) {
     let generateLocation = document.getElementById("navbar");
 
     generateLocation.innerHTML = "" +
     "    <div class=\"buttonLeft\">\n" +
     "        <div class=\"primaryColor navbarText logoButton\">\n" +
-    "            <img id=\"navbarLogo\" src=\"" + pathToMain + "images/logo.svg\" alt=\"logo\">\n" +
+    "            <img id=\"navbarLogo\" src=\"" + pathToPages + "../images/logo.svg\" alt=\"logo\">\n" +
     "        </div>\n" +
     "    </div>\n" +
-    "    <a href=\"" + pathToMain + "contact.html\">\n" +
+    "    <a href=\"" + pathToPages + "contact\">\n" +
     "        <div id=\"contact\" class=\"buttonRight contactButton\">\n" +
     "            <div class=\"primaryColor navbarText textButton\">CONTACT</div>\n" +
     "        </div>\n" +
     "    </a>\n" +
-    "    <a href=\"" + pathToMain + "about.html\">\n" +
+    "    <a href=\"" + pathToPages + "about\">\n" +
     "        <div id=\"about\" class=\"buttonRight aboutButton\">\n" +
     "            <div class=\"primaryColor navbarText textButton\">ABOUT</div>\n" +
     "        </div>\n" +
     "    </a>\n" +
-    "    <a href=\"" + pathToMain + "games.html\">\n" +
+    "    <a href=\"" + pathToPages + "games\">\n" +
     "        <div id=\"games\" class=\"buttonRight gamesButton\">\n" +
     "            <div class=\"primaryColor navbarText textButton\">GAMES</div>\n" +
     "        </div>\n" +
     "    </a>\n" +
-    "    <a href=\"" + pathToMain + "tools.html\">\n" +
+    "    <a href=\"" + pathToPages + "tools\">\n" +
     "        <div id=\"tools\" class=\"buttonRight toolsButton\">\n" +
     "            <div class=\"primaryColor navbarText textButton\">TOOLS</div>\n" +
     "        </div>\n" +
     "    </a>\n" +
     "\n" +
-    "    <a href=\"" + pathToMain + "index.html\">\n" +
+    "    <a href=\"" + pathToPages + "../index\">\n" +
     "        <div id=\"home\" class=\"buttonLeft homeButton\">\n" +
     "            <div class=\"primaryColor navbarText textButton\">HOME</div>\n" +
     "        </div>\n" +
