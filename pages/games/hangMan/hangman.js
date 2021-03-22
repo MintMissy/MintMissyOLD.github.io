c06246437da217d8896a211283fd83a95dd55bed
MintMissy <dawidkostka480@gmail.com>
2021-03-22 23:59:04 +0100

    Add basic detection system to hangman

--- pages/games/hangMan/hangman.js
+++ pages/games/hangMan/hangman.js
@@ -1,4 +1,13 @@
-function generateLetters(){
+// Method to string
+String.prototype.replaceAt = function(index, replacement) {
+    if (index >= this.length) {
+        return this.valueOf();
+    }
+
+    return this.substring(0, index) + replacement + this.substring(index + 1);
+}
+
+function generateLetters() {
     let letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
         'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
@@ -10,18 +19,24 @@ function generateLetters(){
     for (let i = 0; i < letters.length; i++) {
         let row = Math.floor(i / 7);
 
-        switch(row){
+        let classesText = "class=\'letterCell\'";
+        let onClickText = "onclick=\'guessExecutor(this, \"" + letters[i] + "\")\'";
+        let innerHtmlText = letters[i];
+
+        let textToInsert = "<div " + classesText + " " + onClickText + ">" + innerHtmlText + "</div>"
+
+        switch (row) {
             case 0:
-                firstRow.innerHTML += "<div class='letterCell'>" + letters[i] + "</div>";
+                firstRow.innerHTML += textToInsert;
                 break;
             case 1:
-                secondRow.innerHTML += "<div class='letterCell'>" + letters[i] + "</div>";
+                secondRow.innerHTML += textToInsert;
                 break;
             case 2:
-                thirdRow.innerHTML += "<div class='letterCell'>" + letters[i] + "</div>";
+                thirdRow.innerHTML += textToInsert;
                 break;
             case 3:
-                fourthRow.innerHTML += "<div class='letterCell'>" + letters[i] + "</div>";
+                fourthRow.innerHTML += textToInsert;
                 break;
         }
     }
@@ -40,13 +55,69 @@ function drawElement(selector) {
     path.getBoundingClientRect();
 
     path.style.transition = path.style.WebkitTransition =
-        'stroke-dashoffset 1s ease-in-out';
+        'stroke-dashoffset 0.5s ease-in-out';
 
     path.style.strokeDashoffset = '0';
 }
 
-function getRandomWord(){
+function isInWord(letter, word) {
+    return !!word.includes(letter);
+}
+
+function getIndexesInWord(letter, word) {
+    let indexes = [];
+    for (let i = 0; i < word.length; i++) {
+        if (word.charAt(i) === letter) {
+            indexes.push(i).log;
+        }
+    }
+    return indexes;
+}
+
+function getRandomWord() {
     let maximum = wordList.length;
     let randomIndex = Math.floor(Math.random() * (maximum + 1));
     return wordList[randomIndex];
+}
+
+// Generate word and user progress - gue
+let guessedWord = getRandomWord()
+console.log(guessedWord);
+let progress = "";
+for (let i = 0; i < guessedWord.length; i++) {
+    progress += "_"
+}
+
+// Set word on screen to generated word
+let guessedWordDiv = document.getElementsByClassName("guessedWord")[0];
+guessedWordDiv.innerHTML = progress;
+
+// Current guessed letter
+let guess = "";
+
+// Mistakes variable
+let mistakes = 0;
+
+// Remove letter from list, check if guess was correct or wrong.
+function guessExecutor(letterDiv, letter) {
+    // Remove letter div
+    letterDiv.innerHTML = null;
+    letterDiv.removeAttribute("onclick");
+
+    guess = letter;
+
+    isInWord(letter, guessedWord) ? correctGuess() : wrongGuess();
+}
+
+function correctGuess() {
+    let indexesToReplace = getIndexesInWord(guess, guessedWord);
+    for (let i = 0; i < indexesToReplace.length; i++) {
+        progress = progress.replaceAt(indexesToReplace[i], guess)
+    }
+    guessedWordDiv.innerHTML = progress;
+}
+
+function wrongGuess() {
+    mistakes++;
+    drawElement(('.path' + mistakes));
 }
